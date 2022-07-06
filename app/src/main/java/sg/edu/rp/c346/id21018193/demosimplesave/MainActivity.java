0850e7cdc57159b19f0e59654983024cde4d1863
lee zhuo hong <21018193@myrp.edu.sg>
2022-07-06 09:47:21 +0800

    e

--- app/src/main/java/sg/edu/rp/c346/id21018193/demosimplesave/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/demosimplesave/MainActivity.java
@@ -7,17 +7,17 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
 
         String msg = "No Greeting";
         Toast toast = Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG);
         toast.show();
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
